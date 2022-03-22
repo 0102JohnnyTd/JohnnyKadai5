36cb094d23fad544300ca9f9d64b2eec04eb7814
0102johnny <rt2824.recruit.1022@gmail.com>
2022-03-22 20:09:35 +0900

    ・textFieldの入力値が空だった場合の処理の実装 ・右辺のtextFieldの入力値がからだった場合の処理の実装 ・UIAlertControllerを表示させる処理の実装

--- JohnnyKadai5/ViewController.swift
+++ JohnnyKadai5/ViewController.swift
@@ -8,25 +8,48 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    private lazy var values = valueTextFields.map { $0.textToInt }
+    private let firstValueAlert = "割られる数を入力してください"
+    private let secondValueAlert = "割る数を入力してください"
+    private let secondValueIsZeroAlert = "割る数は0以外の値を入力してください"
 
     @IBOutlet private var valueTextFields: [UITextField]!
 
-    @IBOutlet weak private var resultLabel: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
 
     @IBAction private func executeCalculation(_ sender: Any) {
-        let division = values[0] / values[1]
-        resultLabel.text = String(division)
+        checkTextFieldsValue()
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private func checkTextFieldsValue() {
+        switch valueTextFields[0].text {
+        case "":
+            showAlert(messeage: firstValueAlert)
+        default:
+            switch valueTextFields[1].text {
+            case "":
+                showAlert(messeage: secondValueAlert)
+            default:
+                let values = valueTextFields.map { $0.textToDouble }
+                guard !values[1].isZero else {
+                    showAlert(messeage: secondValueIsZeroAlert)
+                    return
+                }
+                resultLabel.text = String(values[0] / values[1])
+            }
+        }
+    }
+
+    private func showAlert(messeage: String) {
+        let alertController = UIAlertController(title: "エラー", message: messeage, preferredStyle: .alert)
+
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+        present(alertController, animated: true, completion: nil)
     }
 }
 
 private extension UITextField {
-    var textToInt: Int {
-        text.flatMap { Int($0) } ?? 0
+    var textToDouble: Double {
+        text.flatMap { Double($0) } ?? 0
     }
 }
